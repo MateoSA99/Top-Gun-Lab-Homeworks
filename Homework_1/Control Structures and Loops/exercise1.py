1f6ff72c5cf684b26724309921b34735cf36fc54
mateosotoa <mateo.soto@symplifica.com>
2023-06-05 01:00:02 -0500

    correction, last was exercise 1

--- Homework_1/Control Structures and Loops/exercise1.py
+++ Homework_1/Control Structures and Loops/exercise1.py
@@ -2,6 +2,7 @@
 Write a Python program that prints the first 10 Fibonacci numbers using a loop.
 """
 
+
 def ask_number_elements() -> int:
     """This functions ask to the user to the number of elements in the fibonacci sequence
     that user wants to see
@@ -16,6 +17,7 @@ def ask_number_elements() -> int:
         except ValueError:
             print("Invalid Input. You must enter a number!")
 
+
 def fibonacci_sequence(number: int):
     """This function return the first numbers of the fibonacci sequence
     The number of elements that will be show is given by the user.
@@ -23,14 +25,15 @@ def fibonacci_sequence(number: int):
     Args:
         number (int): the number of terms of the fibonacci sequence that user wants to see
     """
-    first_elements = [1,1]
+    first_elements = [1, 1]
     if number == 1:
         return print(first_elements[0])
     else:
-        for i in range(2,number):
+        for i in range(2, number):
             next_number = first_elements[i-1] + first_elements[i-2]
             first_elements.append(next_number)
     print(*first_elements)
+
 
 number = ask_number_elements()
 
